ea4b7399151210343070df4f0051eec0f55b952a
Maciej Myśliński <maciej.myslinski@netguru.co>
2019-02-11 21:16:34 +0100

    Write a failing test to reproduce the problem

--- src/OnChange.test.js
+++ src/OnChange.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import TestUtils from 'react-dom/test-utils'
-import { Form } from 'react-final-form'
+import { Form, Field } from 'react-final-form'
+import { render, fireEvent } from 'react-testing-library'
 import OnChange from './OnChange'
 
 const onSubmitMock = () => {}
@@ -68,5 +69,74 @@ describe('OnChange', () => {
     expect(spy).toHaveBeenCalled()
     expect(spy).toHaveBeenCalledTimes(1)
     expect(spy).toHaveBeenCalledWith(null, 'bar')
+  })
+
+  it('should handle quick subsiquent changes properly', async () => {
+    const kids = [
+      {
+        name: 'Boy(s)'
+      },
+      {
+        name: 'Girl(s)'
+      }
+    ]
+    const { getByLabelText } = render(
+      <Form onSubmit={onSubmitMock}>
+        {({ form }) => (
+          <React.Fragment>
+            <Field name="selectAll" type="checkbox">
+              {({ input }) => (
+                <div>
+                  <label htmlFor="both">Both</label>
+                  <input {...input} id="both" type="checkbox" />
+                  <OnChange name="selectAll">
+                    {next => {
+                      if (next) {
+                        return form.change('kids', kids.map(({ name }) => name))
+                      } else {
+                        return form.change('kids', undefined)
+                      }
+                    }}
+                  </OnChange>
+                </div>
+              )}
+            </Field>
+            {kids.length > 0 &&
+              kids.map((kid, index) => {
+                return (
+                  <Field
+                    key={index}
+                    name="kids"
+                    value={kid.name}
+                    type="checkbox"
+                  >
+                    {({ input, meta: { active } }) => (
+                      <div>
+                        <label htmlFor={index}>{kid.name}</label>
+                        <input {...input} id={index} type="checkbox" />
+
+                        <OnChange name="kids">
+                          {next => {
+                            if (active) {
+                              return form.change('selectAll', next.length === 2)
+                            }
+                          }}
+                        </OnChange>
+                      </div>
+                    )}
+                  </Field>
+                )
+              })}
+          </React.Fragment>
+        )}
+      </Form>
+    )
+    const all = getByLabelText('Both')
+    const boys = getByLabelText('Boy(s)')
+    const girls = getByLabelText('Girl(s)')
+    fireEvent.click(boys)
+    fireEvent.click(girls)
+    expect(girls.checked).toBe(true)
+    expect(all.checked).toBe(girls.checked)
   })
 })
